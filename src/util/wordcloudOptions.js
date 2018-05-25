84f8bc1cd6ef71ba6b0dca01425d3c2ed6d65540
Peiwen <Javapeiwen2010@gmail.com>
2018-05-25 09:15:01 +0800

    update wpw

--- src/util/wordcloudOptions.js
+++ src/util/wordcloudOptions.js
@@ -1,7 +1,41 @@
 let setOption = (data, str) => {
-  console.log(data)
   let option = {
-    list: data || [['foo', 12], ['bar', 6]]
+    list: data || [['foo', 12], ['bar', 6]],
+    fontFamily: '"Trebuchet MS", "Heiti TC", "微軟正黑體", ' +
+                '"Arial Unicode MS", "Droid Fallback Sans", sans-serif',
+    fontWeight: 'normal',
+    color: 'random-dark',
+    minSize: 0, // 0 to disable
+    weightFactor: 1,
+    clearCanvas: true,
+    backgroundColor: 'rgba(255,0,0,0)', // opaque white = rgba(255, 255, 255, 1)
+
+    gridSize: 8,
+    drawOutOfBound: false,
+    origin: null,
+
+    drawMask: false,
+    maskColor: 'rgba(255,0,0,0.3)',
+    maskGapWidth: 0.3,
+
+    wait: 0,
+    abortThreshold: 0, // disabled
+    abort: function noop () {},
+
+    minRotation: -Math.PI / 2,
+    maxRotation: Math.PI / 2,
+    rotationSteps: 0,
+
+    shuffle: true,
+    rotateRatio: 0.1,
+
+    shape: 'star',
+    ellipticity: 0.65,
+
+    classes: null,
+
+    hover: null,
+    click: null
   }
   return option
 }
