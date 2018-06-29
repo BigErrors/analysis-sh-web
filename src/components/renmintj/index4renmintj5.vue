8c9cbee192e62798a75b60bb25cf25f7162cb25f
wupeiwen <javapeiwen2010@gmail.com>
2018-06-29 15:25:43 +0800

    update wpw

--- src/components/renmintj/index4renmintj5.vue
+++ src/components/renmintj/index4renmintj5.vue
@@ -2,7 +2,7 @@
  * @Author: wupeiwen javapeiwen2010@gmail.com
  * @Date: 2018-06-29 13:11:45
  * @Last Modified by: wupeiwen javapeiwen2010@gmail.com
- * @Last Modified time: 2018-06-29 13:30:02
+ * @Last Modified time: 2018-06-29 15:25:34
  * @content: echarts 三位地理坐标系 mapbox
  */
 
@@ -328,6 +328,7 @@ export default {
       }
     },
     init () {
+      // test
       const option = {
         visualMap: [{
           type: 'continuous',
