7cbf55774af7b98d181b7fee4d76a8c3c7ac26cc
wupeiwen <javapeiwen2010@gmail.com>
2018-08-03 10:21:48 +0800

    构建时使用gzip压缩代码；
    Signed-off-by: wupeiwen <javapeiwen2010@gmail.com>

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '*':{
-        target: 'http://192.168.22.137:8860',
+        target: 'http://139.224.131.57:8888',
         changeOrigin:true
       }
     },
@@ -60,7 +60,7 @@ module.exports = {
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
@@ -68,7 +68,7 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
